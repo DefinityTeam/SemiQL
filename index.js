af585a5c5ee9463a42235197f1c1f0850253a33d
Roux S <roux@sudocode1.xyz>
2021-11-12 18:17:52 +0000

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-const YAML = require('yaml');
-const fs = require('fs');
-const semiql = require('./semiql');
-
-let config = YAML.parse(fs.readFileSync('config.yml', 'utf8'));
-let requests = YAML.parse(fs.readFileSync('requests.yml', 'utf8'));
-
-semiql.createSemiQLAPI(config, requests);
